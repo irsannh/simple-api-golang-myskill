a48d61847d91e5e06aa6e9a65c2c1a4a0d78e845
Irsan Nur Hidayat <irsannur.h@gmail.com>
2025-08-05 20:52:13 +0700

    Second Commit, Added GORM

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module simple-api
 
 go 1.23.6
 
+require github.com/lib/pq v1.10.9
+
 require (
 	github.com/bytedance/sonic v1.13.3 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
@@ -14,10 +16,11 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/lib/pq v1.10.9
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
-	"reflect"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 )
 
@@ -18,133 +18,193 @@ type newStudent struct {
 	Student_phone_no string `json:"student_phone_no" binding:"required"`
 }
 
-func rowToStruct(rows *sql.Rows, dest interface{}) error {
-	destv := reflect.ValueOf(dest).Elem()
-	args := make([]interface{}, destv.Type().Elem().NumField())
-	for rows.Next() {
-		rowp := reflect.New(destv.Type().Elem())
-		rowv := rowp.Elem()
+// func rowToStruct(rows *sql.Rows, dest interface{}) error {
+// 	destv := reflect.ValueOf(dest).Elem()
+// 	args := make([]interface{}, destv.Type().Elem().NumField())
+// 	for rows.Next() {
+// 		rowp := reflect.New(destv.Type().Elem())
+// 		rowv := rowp.Elem()
 
-		for i := 0; i < rowv.NumField(); i++ {
-			args[i] = rowv.Field(i).Addr().Interface()
-		}
+// 		for i := 0; i < rowv.NumField(); i++ {
+// 			args[i] = rowv.Field(i).Addr().Interface()
+// 		}
 
-		if err := rows.Scan(args...); err != nil {
-			return err
-		}
+// 		if err := rows.Scan(args...); err != nil {
+// 			return err
+// 		}
 
-		destv.Set(reflect.Append(destv, rowv))
-	}
+// 		destv.Set(reflect.Append(destv, rowv))
+// 	}
 
-	return nil
+// 	return nil
+// }
+
+func postHandler(ctx *gin.Context, db *gorm.DB) {
+	
+	// if ctx.Bind(&newStudent) == nil {
+	// 	_, err := db.Exec("INSERT INTO students VALUES ($1, $2, $3, $4, $5)", newStudent.Student_id, newStudent.Student_name, newStudent.Student_age, newStudent.Student_address, newStudent.Student_phone_no)
+	// 	if err != nil {
+	// 		ctx.JSON(http.StatusBadRequest, gin.H{
+	// 			"message": err.Error(),
+	// 		})
+	// 		return
+	// 	}
+
+	// 	ctx.JSON(http.StatusOK, gin.H{
+	// 		"message":"success create",
+	// 	})
+	// 	return
+	// }
+
+	// ctx.JSON(http.StatusBadRequest, gin.H{
+	// 	"message": "Error",
+	// })
+	var newStudent newStudent
+
+	ctx.Bind(&newStudent)
+	db.Create(&newStudent)
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Success Create",
+		"data": newStudent,
+	})
 }
 
-func postHandler(ctx *gin.Context, db *sql.DB) {
-	var newStudent newStudent
-	if ctx.Bind(&newStudent) == nil {
-		_, err := db.Exec("INSERT INTO students VALUES ($1, $2, $3, $4, $5)", newStudent.Student_id, newStudent.Student_name, newStudent.Student_age, newStudent.Student_address, newStudent.Student_phone_no)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{
-				"message": err.Error(),
-			})
-			return
-		}
+func getAllHandler(ctx *gin.Context, db *gorm.DB) {
+	
+	// row, err := db.Query("SELECT * FROM students")
+	// if err != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, gin.H{
+	// 		"error":err.Error(),
+	// 	})
+	// }
 
-		ctx.JSON(http.StatusOK, gin.H{
-			"message":"success create",
+	// rowToStruct(row, &newStudent)
+	// if newStudent == nil {
+	// 	ctx.JSON(http.StatusNotFound, gin.H{
+	// 		"message": "Not Found",
+	// 	})
+	// 	return
+	// }
+
+	// ctx.JSON(http.StatusOK, gin.H{
+	// 	"data": newStudent, 
+	// })
+
+	var newStudent []newStudent
+
+	db.Find(&newStudent)
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Success Find All",
+		"data": newStudent,
+	})
+	
+}
+
+func getHandler(ctx *gin.Context, db *gorm.DB) {
+	// var newStudent []newStudent
+	// studentId := ctx.Param("student_id")
+	// row, err := db.Query("SELECT * FROM students WHERE student_id = $1", studentId)
+	// if err != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, gin.H{
+	// 		"error":err.Error(),
+	// 	})
+	// }
+
+	// rowToStruct(row, &newStudent)
+
+	// if newStudent == nil {
+	// 	ctx.JSON(http.StatusNotFound, gin.H{
+	// 		"message": "Not Found",
+	// 	})
+	// 	return
+	// }
+
+	// ctx.JSON(http.StatusOK, gin.H{
+	// 	"data": newStudent, 
+	// })
+
+	var newStudent newStudent
+	studentId := ctx.Param("student_id")
+	
+	if db.Find(&newStudent, "student_id=?", studentId).RecordNotFound() {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": "Data Not Found",
 		})
 		return
 	}
 
-	ctx.JSON(http.StatusBadRequest, gin.H{
-		"message": "Error",
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Success Find By Id",
+		"data": newStudent,
 	})
 }
 
-func getAllHandler(ctx *gin.Context, db *sql.DB) {
-	var newStudent []newStudent
-	row, err := db.Query("SELECT * FROM students")
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error":err.Error(),
-		})
-	}
+func putHandler(ctx *gin.Context, db *gorm.DB) {
+	// studentId := ctx.Param("student_id")
+	// var newStudent newStudent
+	// if ctx.Bind(&newStudent) == nil {
+	// 	_, err := db.Exec("UPDATE students SET student_name = $1 WHERE student_id = $2", newStudent.Student_name, studentId)
+	// 	if err != nil {
+	// 		ctx.JSON(http.StatusInternalServerError, gin.H{
+	// 			"error": err.Error(),
+	// 		})
+	// 	}
 
-	rowToStruct(row, &newStudent)
-	if newStudent == nil {
+	// 	ctx.JSON(http.StatusOK, gin.H{
+	// 		"message": "Success Update",
+	// 	})
+	// }
+
+	
+	var newStudent = newStudent{}
+	studentId := ctx.Param("student_id")
+
+	if db.Find(&newStudent, "student_id=?", studentId).RecordNotFound() {
 		ctx.JSON(http.StatusNotFound, gin.H{
 			"message": "Not Found",
 		})
 		return
 	}
-
+	var reqStudent = newStudent
+	ctx.Bind(&reqStudent)
+	db.Model(&newStudent).Where("student_id=?", studentId).Update(reqStudent)
 	ctx.JSON(http.StatusOK, gin.H{
-		"data": newStudent, 
+		"message": "Success Update Data",
+		"data": reqStudent,
 	})
 }
 
-func getHandler(ctx *gin.Context, db *sql.DB) {
-	var newStudent []newStudent
-	studentId := ctx.Param("student_id")
-	row, err := db.Query("SELECT * FROM students WHERE student_id = $1", studentId)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error":err.Error(),
-		})
-	}
+func deleteHandler(ctx *gin.Context, db *gorm.DB) {
+	// studentId := ctx.Param("student_id")
 
-	rowToStruct(row, &newStudent)
+	// _, err := db.Exec("DELETE FROM students WHERE student_id = $1", studentId)
+	// if err != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, gin.H{
+	// 		"error": err.Error(),
+	// 	})
+	// }
 
-	if newStudent == nil {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"message": "Not Found",
-		})
-		return
-	}
+	// ctx.JSON(http.StatusOK, gin.H{
+	// 	"message": "Success Delete",
+	// })
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"data": newStudent, 
-	})
-}
-
-func putHandler(ctx *gin.Context, db *sql.DB) {
-	studentId := ctx.Param("student_id")
 	var newStudent newStudent
-	if ctx.Bind(&newStudent) == nil {
-		_, err := db.Exec("UPDATE students SET student_name = $1 WHERE student_id = $2", newStudent.Student_name, studentId)
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Success Update",
-		})
-	}
-}
-
-func deleteHandler(ctx *gin.Context, db *sql.DB) {
 	studentId := ctx.Param("student_id")
-
-	_, err := db.Exec("DELETE FROM students WHERE student_id = $1", studentId)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-	}
-
+	db.Delete(&newStudent, "student_id=?", studentId)
 	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Success Delete",
+		"message": "Success Delete Data",
 	})
 }
 
 func setupRouter() *gin.Engine {
 	conn := "postgres://postgres:postgres@127.0.0.1/postgres?sslmode=disable"
-	db, err := sql.Open("postgres", conn)
+	db, err := gorm.Open("postgres", conn)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	Migrate(db)
+
 	r := gin.Default()
 
 	r.POST("/student", func(ctx *gin.Context) {
@@ -174,4 +234,26 @@ func main() {
 	r := setupRouter()
 
 	r.Run("localhost:8080")
+}
+
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(&newStudent{})
+
+	data := newStudent{}
+	if db.Find(&data).RecordNotFound() {
+		fmt.Print("=============== run seeder ===============")
+		seederUser(db)
+	}
+}
+
+func seederUser(db *gorm.DB) {
+	data := newStudent{
+		Student_id: 1,
+		Student_name: "Irsan Nur Hidayat",
+		Student_age: 23,
+		Student_address: "Jakarta",
+		Student_phone_no: "081234567890",
+	}
+
+	db.Create(&data)
 }
